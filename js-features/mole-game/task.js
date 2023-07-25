0f7092ac5f10376beb27445858030eafae58f25f
Natasha <trofne@mail.ru>
2023-07-25 12:22:28 +0300

    убей кротов готово

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,37 @@
+function getHole(index) {
+    return document.getElementById(hole${index});
+  }
+  
+  let score = 0;
+  let misses = 0;
+  
+  function registerHoleHandlers() {
+    for (let i = 1; i <= 9; i++) {
+      const hole = getHole(i);
+      hole.addEventListener('click', () => {
+        if (hole.classList.contains('hole_has-mole')) {
+          score++;
+          document.getElementById('dead').textContent = score;
+          if (score === 10) {
+            alert('Вы победили!');
+            score = 0;
+            misses = 0;
+            document.getElementById('dead').textContent = score;
+            document.getElementById('lost').textContent = misses;
+          }
+        } else {
+          misses++;
+          document.getElementById('lost').textContent = misses;
+          if (misses === 5) {
+            alert('Вы проиграли!');
+            score = 0;
+            misses = 0;
+            document.getElementById('dead').textContent = score;
+            document.getElementById('lost').textContent = misses;
+          }
+        }
+      });
+    }
+  }
+  
+  registerHoleHandlers();
