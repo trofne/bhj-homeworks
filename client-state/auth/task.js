0e33af04a9e2e0735504e1af68506a2b81f182c6
Natasha <trofne@mail.ru>
2023-08-07 22:26:10 +0300

    Авторизация готова

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,8 +1,35 @@
-{
-  "id": 123,
-  "name": "Иван",
-  "surname": "Иванов",
-  "email": "ivanov@example.com"
-}
+const signInBtn = document.getElementById("signin__btn");
+const form = document.getElementById("signin__form");
+const signInContainer = document.getElementById("signin");
+const welcomeContainer = document.getElementById("welcome");
+const userIdSpan = document.getElementById("user_id");
 
-localStorage.setItem('user', JSON.stringify(user));
+signInBtn.onclick = () => {
+  signIn();
+};
+
+function signIn() {
+  const request = new XMLHttpRequest();
+  request.open(
+    "POST",
+    "https://students.netoservices.ru/nestjs-backend/auth",
+    true
+  );
+  request.responseType = "json";
+  request.onreadystatechange = () => {
+    if (request.status != 200 && request.status != 201) {
+      return;
+    }
+    const data = request.response;
+    if (data.success) {
+      signInContainer.classList.remove("signin_active");
+      welcomeContainer.classList.add("welcome_active");
+      userIdSpan.textContent = data.user_id;
+    } else {
+      alert("Неверные логин/пароль");
+    }
+  };
+
+  const formData = new FormData(form);
+  request.send(formData);
+}
