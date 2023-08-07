88ff1f8074782d01beb1412d55a19fc8a0dceeed
Natasha <trofne@mail.ru>
2023-08-07 21:42:36 +0300

    Опрос с выбором результатов готов

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,18 +1,25 @@
 const pollTitle = document.getElementById("poll__title");
 const pollAnswers = document.getElementById("poll__answers");
 
-// Загрузка опроса
-fetch("https://students.netoservices.ru/nestjs-backend/poll")
-  .then((response) => response.json())
-  .then((data) => {
-    pollTitle.textContent = data.data.title;
-    data.data.answers.forEach((answer) => {
-      const answerButton = document.createElement("button");
-      answerButton.classList.add("poll__answer");
-      answerButton.textContent = answer;
-      answerButton.addEventListener("click", () => {
-        alert("Спасибо, ваш голос засчитан!");
+function fetchPollData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll", true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      pollTitle.textContent = data.data.title;
+      data.data.answers.forEach((answer) => {
+        const answerButton = document.createElement("button");
+        answerButton.classList.add("poll__answer");
+        answerButton.textContent = answer;
+        answerButton.addEventListener("click", () => {
+          alert("Спасибо, ваш голос засчитан!");
+        });
+        pollAnswers.appendChild(answerButton);
       });
-      pollAnswers.appendChild(answerButton);
-    });
-  });
+    }
+  };
+  xhr.send();
+}
+
+fetchPollData();
